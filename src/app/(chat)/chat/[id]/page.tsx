fff301f489acaace0f3630f3631d9c6100b2cdd7
Vucongchien <vucongchien204@gmail.com>
2025-11-12 15:35:34 +0700

    fetch message in client component

--- src/app/(chat)/chat/[id]/page.tsx
+++ src/app/(chat)/chat/[id]/page.tsx
@@ -7,30 +7,9 @@ import { ChatPageClient } from "./chat-page-client";
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
   const { id } = params;
-  const [userId, token] = await Promise.all([
-    getUidFromCookie(),
-    getTokenFromCookie(),
-  ]);
-  console.log("userId", userId,token)
-
-  if (!userId || !token) {
-    notFound();
-  }
-
-  const sessionMessages = await fetchSessionMessages(
-    id,
-    "copilot-chan",
-    userId,
-    token
-  );
-  console.log("[chat/[id]]",sessionMessages);
   return (
     <>
-
-    <ChatPageClient
-      threadId={id}
-      initialMessages={sessionMessages}
-      />
+      <ChatPageClient threadId={id} />
     </>
   );
 }
